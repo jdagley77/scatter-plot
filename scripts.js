c4f6a8e8e6d9a5ce2e1fed614efbdc6187156661
Jack Dagley <jack.dagley77@gmail.com>
2018-07-25 09:39:18 -0700

    clean up code and update labels

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-
+// request the data
 var api = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 document.addEventListener("DOMContentLoaded", function(event) {
    fetch(api)
      .then(function(response) { return response.json(); })
      .then(function(data) { 
-         //DO SOMETHING WITH DATA  
+         //do something with the data 
          parsedData = parseData(data)
          drawScatterPlot(parsedData)
      })
@@ -12,19 +12,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 // formats the data
 function parseData(json) {
-	console.log(json)
 	var arr = [];
 	json.forEach(function(datapoint){
 		var tempObj  = {
-			'Time': new Date(datapoint['Seconds'] * 1000),
+			'Time': new Date(datapoint['Seconds'] * 1000), //convert from seconds to milliseconds to use d3 time formatter function
 			'Place': datapoint['Place'],
 			'Seconds': datapoint['Seconds'],
 			'Name': datapoint['Name'],
-			'Year': d3.timeParse("%Y")(datapoint['Year']).getFullYear(),
+			'Year': d3.timeParse("%Y")(datapoint['Year']),
 			'Nationality': datapoint['Nationality'],
 			'Doping': datapoint['Doping'],
-			'URL': datapoint['URL'],
-			'Doping': datapoint['Doping']
+			'URL': datapoint['URL'] ? datapoint['URL'] : 'undefined',
+			'Doping': datapoint['Doping'] 
 		}
 		arr.push(tempObj)
 	})
@@ -32,35 +31,29 @@ function parseData(json) {
 	return arr;
 }
 
-function formatTime(time) {
-	parseDate = d3.timeFormat("%Y")(time)
- 	return parseDate;
-}
-	
-
 function drawScatterPlot(data) {
 	const w = 960;
 	const h = 500;
-	const padding = 50;
+	const padding = 100;
 
-	var yScale = d3.scaleLinear()
+	yScale = d3.scaleLinear()
 		.range([h, 0])
 		.domain([d3.max(data, (d) => d.Time), d3.min(data, (d) => d.Time)])
 
-	var xScale = d3.scaleLinear()
+	xScale = d3.scaleLinear()
 		.range([0, w])
 		.domain([d3.min(data, (d)=> d.Year), d3.max(data, (d)=> d.Year)])
 
-	var chart = d3.select('body')
+	chart = d3.select('body')
 		.append('svg')
 		.attr('height', h)
 		.attr('width', w)
 		.style('padding', padding)
 
-	var tooltip = d3.select('body').append('div')
+	tooltip = d3.select('body').append('div')
 		.attr('class', 'tooltip')
 
-	chart.selectAll('circle')
+	circles = chart.selectAll('circle')
 		.data(data)
 		.enter()
 		.append('circle')
@@ -73,18 +66,26 @@ function drawScatterPlot(data) {
 				.style('left', d3.event.pageX + 20 + 'px')
 				.style('top', d3.event.pageY + 'px')
 				.style('display', 'inline-block')
-				.html(d.Name + '<br>' + 
-							d.Nationality + '<br>' + 
-							'Place: ' + d.Place + '<br>' + 
-							d.Time.getMinutes() + ':' + 
-							d.Time.getSeconds() + '<br>'+ 
-							d.Year
-						)
+				.html(
+							`<a href=${d.URL} class='wiki-link' target='_blank'>${d.Name}</a><br> 
+							${d.Nationality}<br> 
+							Place: ${d.Place}<br> 
+							${d.Time.getMinutes()}: ${d.Time.getSeconds()}<br> 
+							${d.Year.getFullYear()}`
+				)
 		})
-		.on('mouseout', function(d) {
+		.on('click', function(d) {
 			tooltip
 				.style('display', 'none')
 		})
+
+	circles.each(function(d) {
+		if (d.Doping.length) {
+			this.classList.add('doping')
+		} else {
+			this.classList.add('not-doping')
+		}
+	})
 
 	const xAxis = d3.axisBottom(xScale)
 	const yAxis = d3.axisLeft(yScale)
@@ -94,10 +95,29 @@ function drawScatterPlot(data) {
 		.attr('transform', 'translate(0,' + h + ')')
 		.call(xAxis.tickFormat(d3.timeFormat("%Y")))
 
+	chart.append('text')
+		.attr('transform', 'rotate(-90)')
+		.attr('y', 0 - padding/2)
+		.attr('x', '-'+(h/2))
+		.text('Time in Minutes')
+
 	chart.append('g')
 		.attr('class', 'yaxis')
 		.attr('transform', 'translate(0,0)')
 		.call(yAxis.tickFormat(d3.timeFormat("%M:%S")));
+
+	chart.append('text')
+		.attr('transform', 'translate('+w/2+', 0)')
+		.attr('class', 'title')
+		.style('text-anchor', 'middle')
+		.text(`Doping in Professional Bicycle Racing`)
+
+	chart.append('text')
+		.attr('transform', 'translate('+w/2+', 30)')
+		.attr('class', 'sub-title')
+		.style('text-anchor', 'middle')
+		.text(`35 Fastest Times up Alpe d'Huez`)
 }
+
 
 
